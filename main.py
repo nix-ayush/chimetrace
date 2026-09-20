4591dc99e3b32cd50fd1af69c762f2f113c72388
Ayush Kumar <266145228+nix-ayush@users.noreply.github.com>
2026-09-20 16:53:24 +0530

    feat: add nested logic checks and default CONFIG configuration
    - Check if channels, sample width and framerate match step-by-step.
    - Add CONFIG enum for fallbacks and as a default config.
    - Add 'pass' placeholders where the audio data math will go later.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from pathlib import Path
+from array import array
+from enum import Enum
 import wave
+
+class CONFIG(Enum):
+    MIXING="UP"
+    SCALING="UP"
+    SAMPLING="UP"
+
+    TARGET_MIXING = 2 # stereo has 2 channels
+    TARGET_SCALING = 2 # ...bytes(int16/16-bit)
+    TARGET_FRAMERATE = 48 * 1000 # (48kHz since default SAMPLING is UP)
+    
 
 def main():
     BASE_DIR = Path("samples")
@@ -9,8 +21,30 @@ def main():
 
     with wave.open(str(main_audio_path), "rb") as main_audio, \
         wave.open(str(chime_audio_path), "rb") as chime_audio:
-        print("Duration of main_audio", get_duration(main_audio.getnframes(), main_audio.getframerate()), "seconds")
-        print("Duration of chime_audio", get_duration(chime_audio.getnframes(), chime_audio.getframerate()), "seconds")
+        # print("Duration of main_audio", get_duration(main_audio.getnframes(), main_audio.getframerate()), "seconds")
+        # print("Duration of chime_audio", get_duration(chime_audio.getnframes(), chime_audio.getframerate()), "seconds")
+
+        # checking if both are equal channel (stereo/mono):
+        if main_audio.getnchannels() == chime_audio.getnchannels():
+            # now since we have equal channels, we check sample-width is equal
+            if main_audio.getsampwidth() == chime_audio.getsampwidth():
+                # now we have equal bit depth, we check framerate is equal
+                if main_audio.getframerate() == chime_audio.getframerate():
+                    pass
+                else:
+                    # [#A] TODO Do upsampling or downsampling
+                    # [#B] TODO provide option/config for either of the up/down-sampling
+                    pass
+                pass
+            else:
+                # [#A] TODO Do upscaling or downscaling
+                # [#B] TODO provide option/config for either of the up/down-scaling
+                pass
+        else:
+            # [#A] TODO Do down-mixing(quality reduce) or up-mixing(quality-preserved)
+            # [#B] TODO provide option/config for either of the up/down-mixing
+            pass
+
 
 def get_duration(frames, framerate):
     return frames / framerate
